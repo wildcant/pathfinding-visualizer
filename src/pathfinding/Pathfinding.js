c7f2c51912c2a18cce2ca7b801acea8a4ff3b780
CwirL <cwilmer@uninorte.edu.co>
2020-01-05 20:13:23 -0500

    fix: node grid on state and resize handler

--- src/pathfinding/Pathfinding.js
+++ src/pathfinding/Pathfinding.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Node from '../node/Node';
-import { withWindowSize } from 'react-fns';
 import './pathfinding.css';
   
 class Pathfinding extends React.Component {
   constructor(props){
     super(props);
+    this.matrix= [];
     this.state = {
-      matrix: []
+      matrix: null
     }
-    this.nodeAtrr = {
-      nodeWidth: 60,
-      nodeHeight: 60,
+    this.nodeSize = {
+      nodeWidth: 80,
+      nodeHeight: 80
     }
-    this.orientation = 'start';
     this.isDrawing = false;
     this.previousNode = null;
     this.mouseEvents = {
@@ -27,11 +26,10 @@ class Pathfinding extends React.Component {
     let isNormal = currentNode.className.search('normal')  !== -1;
     if (this.isDrawing && isNormal && currentNode !== this.previousNode){
       let isWall = currentNode.className.search('wall') !== -1;
-      if (isWall) {
-      currentNode.classList.remove("wall");
-      } else {
-        currentNode.classList.add("wall");
-      }
+      if (isWall)
+        currentNode.classList.remove("wall")
+      else
+        currentNode.classList.add("wall")
       this.previousNode = currentNode
     }
   }
@@ -50,46 +48,51 @@ class Pathfinding extends React.Component {
       this.setWall(currentNode);
     }
   }
+  createNode = (nodePos, i, j) => {
+    return <Node nodePos={nodePos} zInd={i+j} key={`node${i}${j}`}></Node>;
+  }
+
   initNodeMatrix = (cols, rows) => {
     let matrix = new Array(rows);
     let delta = 1/(rows/2);
     let x = 0;
-    for (let i = 0; i < rows*2; i++) {
+    for (let i = 0; i < rows; i++) {
       matrix[i] = new Array(cols);
-      if (i < Math.floor(rows*2/4)) {
-        this.orientation = 'start';
-        x +=delta;
-      } else if (i > Math.floor(rows*3/4)) {
-        this.orientation = 'end';
-        x-=delta;
-      } else {
-        x = 0.98;
-      }
-      console.log(Math.floor(cols*x));
-      for (let j = 0; j < Math.floor(cols*x); j++){
+      if (i < Math.floor(rows*3/5)) {x +=delta} 
+      else if (i > Math.floor(rows*3/5)) {x-=delta} 
+      else {x = 1}
+      for (let j = 0; j < Math.ceil(cols*x); j++){
         let initPoint = i === Math.floor(rows/2) && j === Math.floor(cols*0.1);
-        let endPoint =  i === Math.floor(rows/2) && j === Math.floor(cols*0.8);
-        if (initPoint){
-          matrix[i][j] =  <Node nodePos={"init-point"} zInd={i+j} key={`node${i}${j}`}></Node>
-        } else if (endPoint){
-          console.log("endpoint")
-          matrix[i][j] = <Node nodePos={'end-point'} zInd={i+j} key={`node${i}${j}`}></Node>;
-        } else {
-          matrix[i][j] = <Node nodePos={`normal${i}${j}`} zInd={i+j} key={`node${i}${j}`}></Node>;
-        }
+        let endPoint =  i === Math.floor(rows/2) && j === Math.floor(cols*0.9);
+        if (initPoint)
+          matrix[i][j] =  this.createNode('init-point', i, j);
+        else if (endPoint)
+          matrix[i][j] = this.createNode('end-point', i, j);
+        else
+          matrix[i][j] = this.createNode(`normal${i}${j}`, i, j);
       }
-      matrix[i] = React.createElement('div', {className: 'row '+ this.orientation, key: i}, matrix[i]);
     }
     return matrix;
   }
-  setNodeMatrix = () => {
-    const { nodeWidth, nodeHeight } = this.nodeAtrr;
-    let {width, height} = this.props;
+  
+  setNodeMatrix = (height, width) => {
+    const { nodeWidth, nodeHeight } = this.nodeSize;
     let cols = Math.floor(width/nodeWidth);
-    let rows = Math.floor(height/nodeHeight);
-    console.log(cols, rows);
+    let rows = Math.floor(height/nodeHeight)*2;
     let matrix = this.initNodeMatrix(cols, rows);
     return matrix;
+  }
+
+  handleResize = () => {
+    const {innerHeight, innerWidth} = window;
+    let matrix= this.setNodeMatrix(innerHeight, innerWidth);
+    this.setState({matrix: matrix})
+  } 
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+    const {innerHeight, innerWidth} = window;
+    let matrix= this.setNodeMatrix(innerHeight, innerWidth);
+    this.setState({matrix: matrix})
   }
   render() {
     return ( 
@@ -99,9 +102,14 @@ class Pathfinding extends React.Component {
       onMouseUp={this.handleDrawing} 
       onMouseLeave={this.handleDrawing}
         className="matrix">
-          {this.setNodeMatrix()}
+          {this.state.matrix === null ? 'Loading' :
+          this.state.matrix.map((row, rowId, rows) => {
+            if (rowId > rows.length/2)
+              return <div className='row end' key={rowId}>{row}</div>
+            return <div className='row start' key={rowId}>{row}</div>
+          })}
       </div>
     )
   }
 }
-export default withWindowSize(Pathfinding);
+export default Pathfinding;
